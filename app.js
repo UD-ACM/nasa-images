072cfbf283d8f86d35e6b22020e1a8b51e277f0b
Jinay Jain <jinaybjain@gmail.com>
2021-03-03 15:02:29 -0500

    Add non-demo api key

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function updateObject() {
-    fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=1').then(res => res.json()).then(
+    fetch('https://api.nasa.gov/planetary/apod?api_key=lTE751UXmg73sa2nd1kAQwZyznTxgkMCaH6gbsW8&count=1').then(res => res.json()).then(
         res => {
             let spaceObject = res[0];
             let title = document.getElementById('image-title')
